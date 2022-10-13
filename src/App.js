a320b1d4c26517267904184bf754f1ab9eb4cdef
Omraj <omraj7413@gmail.com>
2022-10-13 21:45:53 +0530

    header section done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
     <div>
       <Navigation/>
+      <Header/>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className='header-container'>
+        <div className='header-img'>
+        </div>
+    </div>
+  )
+}
+
+export default Header
